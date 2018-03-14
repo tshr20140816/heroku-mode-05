ebde7d9c32235027c0c8ffb97af16de4f04d6ce6
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-15 05:10:33 +0900

    Update start_web.sh

--- start_web.sh
+++ start_web.sh
@@ -12,11 +12,6 @@ cat /proc/version
 curl --version
 printenv
 
-if [ ! -v LOGGLY_TOKEN ]; then
-  echo "Error : LOGGLY_TOKEN not defined."
-  exit
-fi
-
 if [ ! -v LOG_LEVEL ]; then
   export LOG_LEVEL="warn"
 fi
