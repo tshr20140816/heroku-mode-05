7718192bc25df4ab40c4ed6738c87ed1cfdd6d81
tshr20140816 <tshr20140816@yahoo.co.jp>
2018-03-16 09:34:33 +0900

    Update post_install.sh

--- 80_install/post_install.sh
+++ 80_install/post_install.sh
@@ -12,6 +12,23 @@ chmod +x dropbox.py
 
 wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 
+postgres_user=$(echo ${DATABASE_URL} | awk -F':' '{print $2}' | sed -e 's/\///g')
+postgres_password=$(echo ${DATABASE_URL} | grep -o '/.\+@' | grep -o ':.\+' | sed -e 's/://' | sed -e 's/@//')
+postgres_server=$(echo ${DATABASE_URL} | awk -F'@' '{print $2}' | awk -F':' '{print $1}')
+postgres_dbname=$(echo ${DATABASE_URL} | awk -F'/' '{print $NF}')
+
+export PGPASSWORD=${postgres_password}
+
+psql -U ${postgres_user} -d ${postgres_dbname} -h ${postgres_server} > /tmp/sql_result.txt << __HEREDOC__
+CREATE TABLE t_downloads
+(
+ url character varying(512) PRIMARY KEY
+,file_name character varying(255) NOT NULL
+,status int
+,create_time timestamp DEFAULT localtimestamp NOT NULL
+)
+__HEREDOC__
+cat /tmp/sql_result.txt
 
 echo ${start_date}
 date
